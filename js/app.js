595cde6e9fe32562ad9eb5cb907aab6012b4ec01
Anthony Liu <tlrnalra@gmail.com>
2016-01-28 06:49:05 -0500

    landing page

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 60
+            scrollTop: $($anchor.attr('href')).offset().top - 80
         }, 700, 'easeInOutExpo');
         event.preventDefault();
     });
