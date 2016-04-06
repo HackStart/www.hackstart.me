ff649848ad735fd49f588d6c7f649af00352ecab
Anthony Liu <tlrnalra@gmail.com>
2016-04-06 12:29:33 -0400

    asd

--- js/all.js
+++ js/all.js
@@ -3,7 +3,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 80
+            scrollTop: $($anchor.attr('href')).offset().top - window.innerHeight*0.2
         }, 700, 'easeInOutExpo');
         event.preventDefault();
     });
