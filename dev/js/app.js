4a2b4f5054cc565523a8076eedf70c569b3b978c
Anthony Liu <tlrnalra@gmail.com>
2016-04-07 16:59:50 -0400

    asdj

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,10 +1,45 @@
-
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - window.innerHeight*0.15
-        }, 700, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  'use strict';
+  var $page = $('#main'),
+    options = {
+      debug: true,
+      prefetch: true,
+      cacheLength: 2,
+      forms: 'form',
+      onStart: {
+        duration: 400, // Duration of our animation
+        render: function($container) {
+          // Add your CSS animation reversing class
+          $container.addClass('is-exiting');
+          $('html, body').scrollTop();
+          // Restart your animation
+          smoothState.restartCSSAnimations();
+        }
+      },
+      onReady: {
+        duration: 0,
+        render: function($container, $newContent) {
+          // Remove your CSS animation reversing class
+          $container.removeClass('is-exiting');
+          // Inject the new content
+          $container.html($newContent);
+        }
+      },
+      onAfter: function($container, $newContent) {
+        button();
+      }
+    },
+    smoothState = $page.smoothState(options).data('smoothState');
+
+  button();
 });
+
+function button() {
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - window.innerHeight * 0.15
+    }, 700, 'easeInOutExpo');
+    event.preventDefault();
+  });
+}
